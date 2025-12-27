c5e7d74ef633bb8a072e503b2c061a932a49d1b4
ELANKAVI SENTHILRAJA <elankavisenthilraja18@gmail.com>
2025-12-27 22:06:34 +0530

    PurchaseOrderItemService.java

--- PurchaseOrderItemService.java
+++ PurchaseOrderItemService.java
@@ -1,6 +1,5 @@
 package com.examly.springapp.service;
  import java.util.List;
-import java.util.Optional;
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.stereotype.Service;
 import com.examly.springapp.model.PurchaseOrderItem;
@@ -12,7 +11,8 @@ private PurchaseOrderItemRepo purchaseOrderItemRepo;
 public PurchaseOrderItem saveAll(PurchaseOrderItem purchaseOrderItem){
     return purchaseOrderItemRepo.save(purchaseOrderItem);
 }
-// public List<PurchaseOrderItem> getAll(long id) {
-//     return purchaseOrderItemRepo.findByPurchaseOrder_PurchaseOrderId(id);
-// }
+public List<PurchaseOrderItem> getItemByOrderId(Long OrderId){
+    return purchaseOrderItemRepo.findByPurchaseOrderItemId(OrderId);
 }
+}
+
