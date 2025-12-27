a952ec1885d03978ffcb24c8cf6ce02541d07bc4
ELANKAVI SENTHILRAJA <elankavisenthilraja18@gmail.com>
2025-12-27 22:08:57 +0530

    PurchaseOrderItemRepo.java

--- PurchaseOrderItemRepo.java
+++ PurchaseOrderItemRepo.java
@@ -5,5 +5,5 @@ import com.examly.springapp.model.PurchaseOrderItem;
 import java.util.List;
 @Repository
 public interface PurchaseOrderItemRepo extends JpaRepository<PurchaseOrderItem, Long> {
-   //List<PurchaseOrderItem> findByPurchaseOrder_PurchaseOrderId(Long id);
+   List<PurchaseOrderItem> findByPurchaseOrderItemId(Long id);
 }
