8fbee1fb2e5bac002b3b85217826dc547969ee0d
ELANKAVI SENTHILRAJA <elankavisenthilraja18@gmail.com>
2025-12-27 22:12:57 +0530

    SupplierController.java

--- SupplierController.java
+++ SupplierController.java
@@ -1,7 +1,6 @@
 package com.examly.springapp.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
