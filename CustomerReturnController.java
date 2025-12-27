aa70cc3257fac4ae5c93ace9ee5c942d1361730f
ELANKAVI SENTHILRAJA <elankavisenthilraja18@gmail.com>
2025-12-27 22:11:09 +0530

    CustomerReturnController.java

--- CustomerReturnController.java
+++ CustomerReturnController.java
@@ -1,3 +1,4 @@
+
 package com.examly.springapp.controller;
 import org.springframework.http.*;
 import java.util.List;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.examly.springapp.model.CustomerReturn;
